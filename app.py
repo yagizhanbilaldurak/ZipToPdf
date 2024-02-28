118aec2a22f15cf61a80a762ccde111989b52cf9
Yağızhan Bilal Durak <115084674+yagizhanbilaldurak@users.noreply.github.com>
2024-02-28 15:59:08 +0300

    Update app.py
    delete  descriptions

--- app.py
+++ app.py
@@ -19,34 +19,26 @@ def run_program(input_dir, output_dir, pdf_dir, message_text, progress_var):
     output_directory = output_dir.get()
     pdf_directory = pdf_dir.get()
 
-    # Clear previous messages
     message_text.delete('1.0', tk.END)
 
-    # Capture output using StringIO
     captured_output = io.StringIO()
     original_stdout = sys.stdout
     sys.stdout = captured_output
 
     try:
-        # Create an instance of PDFConversionTool
         conversion_tool = PDFConversionTool(input_directory, output_directory, pdf_directory)
 
-        # Call the run_conversion method of PDFConversionTool to start the conversion process
         conversion_tool.run_conversion(progress_var)
 
-        # Display completion message
         print("PDF Conversion Completed!")
     except Exception as e:
-        # Display error message if any exception occurs during the conversion process
         print(f"Error: {str(e)}")
 
     sys.stdout = original_stdout  # Restore original stdout
 
-    # Insert captured output into message_text
     message_text.insert(tk.END, captured_output.getvalue())
 
 def main():
-    # Call the GUI interface
     interface(run_program)
 
 if __name__ == "__main__":
